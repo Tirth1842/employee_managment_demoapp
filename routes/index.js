4d99ed2e0f82333e5b72306dfab62778f528f9e6
Tirth Patel <tirthpatel.1842000@gmail.com>
2022-01-10 16:51:14 +0530

    login credentials set, error display

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,11 @@ const router = express.Router();
 //Welcome Page
 router.get('/', (req,res) => res.render('welcome'));
 
+// Dashboard
+router.get('/dashboard/',ensureAuthenticated,(req,res) => {
+    res.render('dashboard', {
+        name: req.user.name
+    })
+    });
+
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
-const { query } = require('express');
+
 const express = require('express');
-const { append } = require('express/lib/response');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const db = require('../models/User');
 
@@ -57,15 +57,25 @@ router.post('/register',(req,res) => {
         
                 });
                 }else{
-                    var queryConfig = {
-                        text: 'INSERT INTO public."Users"(user_name, email, password) VALUES($1, $2, $3);',
-                        values: [name, email, password]
-                      };
-                    db.query(queryConfig)
-                        .then(()=>{req.flash('success_msg', 'You are now registered and can log in');
-                        res.redirect('/users/login');})
-                        .catch((err) => console.log(err));
-                }
+                    
+                      var newPassword = password;
+                      bcrypt.genSalt(10, (err,salt) => 
+                        bcrypt.hash(newPassword, salt, (err, hash) => {
+                                if(err) throw err;
+                            // set password to hashed
+                                newPassword = hash;
+                                // save user
+                                var queryConfig = {
+                                    text: 'INSERT INTO public."Users"(user_name, email, password) VALUES($1, $2, $3);',
+                                    values: [name, email, newPassword]
+                                  };
+                                db.query(queryConfig)
+                                .then((user)=>{req.flash('success_msg', 'You are now registered and can log in');
+                                        res.redirect('/users/login');})
+                                .catch((err) => console.log(err));
+                                }))
+                            
+                }   
             })
             .catch(err => console.log(err));
         
@@ -74,6 +84,43 @@ router.post('/register',(req,res) => {
 })
 
 // Login render
-
 router.get('/login',(req,res)=>res.render('login'));
+
+// Login User
+router.post('/login',(req,res) => {
+    const{  email, password } = req.body;
+    
+    if(!email || !password){
+        req.flash('error_msg','Please fill the details');
+        res.render('login');
+    }
+    else{
+        db.query('SELECT  email, password FROM public."Users" WHERE email=$1', [email])
+        .then((result) => {
+            
+            if(result.rowCount==0){
+                console.log('hello');
+                req.flash('error_msg1', 'User not registered')
+                res.render('login');
+            }else{
+                // match the password
+                bcrypt.compare(password, result.rows[0].password, (err,isMatch) => {
+                    if(err) throw err;
+
+                    if(isMatch) {
+                        return res.redirect('/dashboard');
+                    } else {
+                        req.flash('error_msg', 'Password incorrect');
+                        res.redirect('/users/login');
+                    }
+            });
+            }
+            
+        })
+        .catch(err => console.log(err));
+    }
+
+    
+
+})
 module.exports = router;
